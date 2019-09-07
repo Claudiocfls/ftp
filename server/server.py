5536bb98f3c6bd30bd72dca6cae5af5795fd4ef1
Claudiocfls <claudiosilva.cfls@gmail.com>
2019-09-07 14:01:22 -0300

    add commands between client and server

--- server/server.py
+++ server/server.py
@@ -46,35 +46,120 @@ class Th(Thread):
         self.conn = conn
         self.addr = addr
         self.state = 1
+        self.server_pwd = subprocess.check_output(["pwd"]).decode()[:-1]
+        self.client_pwd = '/'
 
     def run(self):
         sent = ""
         while True:
-            data = self.conn.recv(1024)
-            if not data:
-                break 
-            sentence = data.decode()
-            print("Received {} from {}".format(sentence, self.addr))
-            received = json.loads(sentence)
-            if self.state == 1:
-                if received["passwd"] == credentials[received["username"]]:
-                    self.conn.send("OKK".encode())
-                    # time.sleep(1)
-                    # self.conn.send(a)
+            com,arg = receiveCommand(self.conn)
+            payload = ''
+            status = 'OK'
+            if com == 'ls':
+                payload = subprocess.check_output(["ls", self.server_pwd + self.client_pwd]).decode()
+            elif com == 'pwd':
+                payload = self.client_pwd
+            elif com == 'mkdir':
+                slash = ''
+                if self.client_pwd[-1] != '/':
+                    slash = '/'
+                subprocess.call(['mkdir',self.server_pwd + self.client_pwd + slash + arg])
+                payload = arg
+            elif com == 'cd':
+                if arg == '..' and self.client_pwd == '/':
+                    status = 'ER'
+                elif arg == '..':
+                    tempDir = self.client_pwd[-1::-1]
+                    slashIndex = tempDir.index('/')
+                    tempDir = tempDir[slashIndex:]
+                    self.client_pwd = tempDir[-1::-1]
+                    if len(self.client_pwd) != 1:
+                        self.client_pwd = self.client_pwd[:-1]
                 else:
-                    self.conn.send("ERR".encode())
-                self.state = 2
-            else:
-                if received["command"] == "get":
-                    # self.conn.send(a)
-                    gRequests.add(self.addr[0], self.addr[1], "download", "image.jpeg")
-                    self.conn.send("OKK".encode())
-                elif received["command"] == "upl":
-                    gRequests.add(self.addr[0], self.addr[1], "upload", "image.jpeg")
-                    self.conn.send("OKK".encode())
+                    if self.client_pwd[-1] == '/':
+                        self.client_pwd += arg 
+                    else:
+                        self.client_pwd += '/' + arg
+                payload = self.client_pwd
+            elif com == 'put':
+                slash = ''
+                if self.client_pwd[-1] != '/':
+                    slash = '/'
+                gRequests.add(self.addr[0], self.addr[1], "upload", self.server_pwd + self.client_pwd + slash + arg)
+            elif com == 'get':
+                slash = ''
+                if self.client_pwd[-1] != '/':
+                    slash = '/'
+                gRequests.add(self.addr[0], self.addr[1], "download", self.server_pwd + self.client_pwd + slash + arg)
+            elif com == 'delete':
+                slash = ''
+                if self.client_pwd[-1] != '/':
+                    slash = '/'
+                a = subprocess.call(['rm',self.server_pwd + self.client_pwd + slash + arg])
+            elif com == 'rmdir':
+                slash = ''
+                if self.client_pwd[-1] != '/':
+                    slash = '/'
+                a = subprocess.call(['rm','-rf', self.server_pwd + self.client_pwd + slash + arg])
+            res = {
+                'status': status,
+                'payload': payload
+            }
+            sendResponse(self.conn, res)
+            # data = self.conn.recv(1024)
+            # if not data:
+            #     break 
+            # sentence = data.decode()
+            # print("Received {} from {}".format(sentence, self.addr))
+            # received = json.loads(sentence)
+            # if self.state == 1:
+            #     if received["passwd"] == credentials[received["username"]]:
+            #         self.conn.send("OKK".encode())
+            #     else:
+            #         self.conn.send("ERR".encode())
+            #     self.state = 2
+            # else:
+            #     if received["command"] == "get":
+            #         # self.conn.send(a)
+            #         gRequests.add(self.addr[0], self.addr[1], "download", "image.jpeg")
+            #         self.conn.send("OKK".encode())
+            #     elif received["command"] == "upl":
+            #         gRequests.add(self.addr[0], self.addr[1], "upload", "image.jpeg")
+            #         self.conn.send("OKK".encode())
 
         self.conn.close()
 
+def sendResponse(conn, res):
+    a = json.dumps(res)
+    conn.send(a.encode())
+
+def receiveCommand(conn):
+    print("recebendo comando")
+    com = conn.recv(1024).decode()
+    print("recebido: ",com)
+    a = json.loads(com)
+    command,argument = a["comm"],a["arg"]
+    return command,argument
+
+def receiveArgument(conn):
+    # conn.settimeout(2)
+    print("timeout padrao ",conn.gettimeout())
+    print("recebendo argumento")
+    serverResponse = ''.encode()
+    while True:
+        try:
+            res = conn.recv(1024)
+            serverResponse += res
+        except:
+            break
+        print("recebido",res.decode(), sys.getsizeof(res))
+        # try:
+        #     resd = res.decode()
+        #     if resd == '  ':
+        #         break 
+        # except:
+        #     serverResponse += res
+    print("argumento decodificado: {}".format(serverResponse.decode()))
 
 class TransferWorker(Thread):
     def __init__(self, addr, connection):
@@ -83,39 +168,36 @@ class TransferWorker(Thread):
         self.addr = addr
 
     def run(self):
-        print("rodando worker")
         mode, filename, ok = gRequests.get(self.addr[0], self.addr[1])
         print(mode,filename)
         if ok:
             if mode == "upload":
-                print("modo upload")
                 self.receiveFile(self.conn, filename)
                 self.conn.close()
             elif mode == "download":
-                print("modo download")
                 self.sendFile(self.conn, filename)
                 self.conn.close()
         else:
             print("ERror")
 
     def sendFile(self, conn, filename):
-        print("starting download")
-        f = open(filename,'rb')
+        print("starting download...")
+        f = open(filename, 'rb')
         chunk = f.read(1024)
         cont = 0
         while (chunk):
             conn.send(chunk)
             cont += 1
-            print('Sent ',repr(chunk))
+            # print('Sent ',repr(chunk))
             chunk = f.read(1024)
         f.close()
-        print("Saiu", cont)
+        print("Download finished...")
     
     def receiveFile(self, conn, filename):
         with open(filename, 'wb') as f:
             print('file opened')
             cont = 0
-            print('receiving data...')
+            print('Receiving data...')
             data = conn.recv(1024)
             while data:
                 cont += 1
@@ -125,25 +207,15 @@ class TransferWorker(Thread):
                 print(cont)
                 data = conn.recv(1024)
             f.close()
-            print("acabou", cont)
-    
-# def handleRequest(request):
-#     a = json.loads(request)
-
+            print("Finished", cont)
 
 def handleNewConnection(addr, connection):
     newThread = Th(addr, connection)
     newThread.start()
 
 def handleNewTransfer(addr, connection):
-    print("criando worker de transferencia")
     newWorker = TransferWorker(addr, connection)
     newWorker.start()
-
-# thread = Th(1)
-# thread.start()
-
-# print(a)
 
 class CommandServer(Thread):
     def __init__(self):
@@ -159,16 +231,7 @@ class CommandServer(Thread):
             connectionSocket, addr = serverSocket.accept()
             handleNewConnection(addr, connectionSocket)
             connectionSocket = None
-            # if commandQueue.get(addr):
-            #     print("Error: connection already exist!")
-            # else:
-            #     commandQueue[addr] = connectionSocket
-            #     print("Thread created {}".format(addr))
-            # sentence = connectionSocket.recv(1024).decode()
-            # print("Received {} from {} - {}".format(sentence,addr,connectionSocket))
             print("New connection established from {}".format(addr))
-            # capitalizedSentence = sentence.upper()
-            # connectionSocket.send(capitalizedSentence.encode())
 
         connectionSocket.close()
 
@@ -186,16 +249,7 @@ class TransferServer(Thread):
         while True:
             connectionSocket, addr = serverSocket.accept()
             handleNewTransfer(addr, connectionSocket)
-            # if commandQueue.get(addr):
-            #     print("Error: connection already exist!")
-            # else:
-            #     commandQueue[addr] = connectionSocket
-            #     print("Thread created {}".format(addr))
-            # sentence = connectionSocket.recv(1024).decode()
-            # print("Received {} from {} - {}".format(sentence,addr,connectionSocket))
             print("New connection established from {} to transfer".format(addr))
-            # capitalizedSentence = sentence.upper()
-            # connectionSocket.send(capitalizedSentence.encode())
 
         connectionSocket.close()
 
@@ -207,7 +261,7 @@ commandServer.start()
 transferServer = TransferServer()
 transferServer.start()
 
-while True:
-    pass
+# while True:
+#     pass
 
 
